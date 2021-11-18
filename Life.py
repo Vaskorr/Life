f3b09c3238a981cc44d50279231f511aee45768d
Vaskorr <basil.suntsov@yandex.ru>
2021-11-18 17:44:20 +0300

    Add input option

--- Life.py
+++ Life.py
@@ -27,7 +27,6 @@ e.g:
 010
 
 TODO: optimize code to run faster
-      add buttons/some different input
       make field boundless (?)
 '''
 
@@ -88,20 +87,41 @@ def drawField():
             elif field[i][j] == 3:
                 field[i][j] = 0
                 gfield[i][j].undraw()
+# game process
+def game():
+    gfield[0][0].draw(win)
+    msg = Text(Point(170, 10), '<- draw figure and click here to start')
+    msg.draw(win)
+    for i in range(1000):
+        a = win.getMouse()
+        x = int(a.x//10)
+        y = int(a.y//10)
+        if x == 0 and y == 0:
+            break;
+        if field[x][y] == 0:
+            field[x][y] = 1
+            gfield[int(a.x//10)][int(a.y//10)].draw(win)
+        else:
+            field[x][y] = 0
+            gfield[int(a.x//10)][int(a.y//10)].undraw()
+    # 1000-steps game
+    for i in range(1000):
+        drawField()
+        a = step()
+        if not(a):
+            break;
+    # the end
+    m = Text(Point(170, 30), 'press any cell to restart')
+    m.draw(win)
+    win.getMouse()
+    m.undraw()
+    return 0
+#restart and clear 
+while(1):
+    game()
+    for i in range(100):
+        for j in range(100):
+            field[i][j] = 0
+            gfield[i][j].undraw()
+            
 
-# glider moving model
-field[50][50] = 1
-field[51][51] = 1
-field[52][51] = 1
-field[52][50] = 1
-field[52][49] = 1
-
-# 1000-steps game
-for i in range(1000):
-    drawField()
-    a = step()
-    if not(a):
-        break;
-# the end
-win.getMouse() 
-win.close()
